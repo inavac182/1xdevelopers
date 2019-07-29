d836257031462fd7cdd9417929fa58dd03bc4ce6
Felipe Nava <felipe.nava.co@gmail.com>
2019-07-29 00:37:33 -0700

    Base code in page and firebase deploy ready

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return <div id='app-container'>
+                    <h1 className='code-text center'>We are </h1>
+                    <h1 className='code-text center'>1x developers</h1>
+                </div>;
+    }
+}
+
+export default App;
